f6040bd76e53433cbbbcbf06b582c7fad6824b01
Chamoli <godchamoli@gmail.com>
2024-09-18 18:25:19 +0530

    chore: Add initial project files and configurations

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,8 @@
+// constants.js
+export const constants = {
+    REGISTER_USER_URL: `${import.meta.env.VITE_BASE_URL}/api/auth/user/v1/register`,
+    LOGIN_USER_URL: `${import.meta.env.VITE_BASE_URL}/api/auth/user/v1/login`,
+    BASE_URL_USER: `${import.meta.env.VITE_BASE_URL}/api/auth/user/v1`,
+    BASE_URL_FLASHCARD: `${import.meta.env.VITE_BASE_URL}/api/auth/flashcard/v1`,
+  };
+  
